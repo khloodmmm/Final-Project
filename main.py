0e6f98d135e28ed357e5930f18432f10008f9799
khloodmmm <khloodm920@gmail.com>
2025-05-02 16:19:43 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-from gui import BookingApp
-import tkinter as tk
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    root.title("Grand Prix Booking System")
-    app = BookingApp(root)
-    root.mainloop()
